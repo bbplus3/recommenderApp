4fb3b737ef99d283931ca67be2b85fe1b8a39e13
Bonnie Bingham <bbplus3@users.noreply.github.com>
2025-03-29 11:10:06 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,1 @@
 from transformers import AutoTokenizer, AutoModel
-
-def load_model():
-    """Load Sentence-BERT model."""
-    model_name = "sentence-transformers/all-MiniLM-L6-v2"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModel.from_pretrained(model_name)
-    return tokenizer, model
-
